778c7c826079ec57ca6690d33f4b748f641ff6f6
= <ismoilbek20050421@gmail.com>
2023-02-19 15:26:03 +0500

    constructor function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-"use strict";
-let count = 0;
+function Counter() {
+  let count = 0;
+  this.increment = function () {
+    count++;
+    result.textContent = count;
+  }
+  this.decrement = function () {
+    if (count <= 0) return;
+    count--;
+    result.textContent = count;
+  }
+  this.reset = function () {
+    count = 0;
+    result.textContent = count;
+  }
+}
 
-const increment = () => {
-  count++;
-  result.textContent = count;
-};
+const counter = new Counter();
 
-const decrement = () => {
-  if (count <= 0) return;
-  count--;
-  result.textContent = count;
-};
-
-const reset = () => {
-  count = 0;
-  result.textContent = count;
-};
+const increment = counter.increment;
+const decrement = counter.decrement;
+const reset = counter.reset;
